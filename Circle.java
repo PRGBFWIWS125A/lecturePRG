b152bf5df61f149e192d8813ebb499a98c67acd5
LoetGurke <118271293+LoetGurke@users.noreply.github.com>
2025-06-26 12:56:33 +0200

    code cleanup

--- Circle.java
+++ Circle.java
@@ -1,22 +1,20 @@
 public class Circle {
+
     private int x;
     private int y;
     private int radius;
 
-public static Circle newCircle(int x, int y, int radius){
-    return new Circle(x, y, radius);
+    private Circle(int x, int y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public static Circle newCircle(int x, int y, int radius) {
+        return new Circle(x, y, radius);
+    }
+
+    public Circle clone() {
+        return new Circle(this.x, this.y, this.radius);
+    }
 }
-
-private Circle(int x, int y, int radius){
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-}
-
-public Circle clone(){
-  return new Circle(this.x, this.y, this.radius);
-}
-
-
-}
-
